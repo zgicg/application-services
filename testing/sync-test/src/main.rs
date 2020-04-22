e317e0d00446fe691a67f07080c6198b470b1c9f
Lina Cambridge <lina@yakshaving.ninja>
2020-04-22 15:47:13 -0700

    Add a basic `sync_multiple` integration test.
    Here's the result of our pair-programming session from today! This
    commit is a whole lot of copypasta from the other integration tests in
    `testing/sync-test`, and the mock `Store` implementation is
    cargo-culted from our other components.
    
    Also, I figured out after why we weren't seeing the incoming records:
    we need to have our `collection_name` be one of the built-in ones, from
    `DEFAULT_ENGINES`. Otherwise, our state machine won't know how to set it
    up. (It's another hack left over from the days where clients would have
    to set the server up again if they got randomly reassigned to a different
    one).
    
    Thanks so much for listening, and for contributing! Please feel free to
    play around with this—try storing and returning new sync IDs, uploading
    invalid records, checking out what the `SyncResult` contains, maybe adding
    a couple of assertions—and we can chat more next time!

--- testing/sync-test/src/main.rs
+++ testing/sync-test/src/main.rs
@@ -8,6 +8,7 @@ use structopt::StructOpt;
 
 mod auth;
 mod logins;
+mod sync15;
 mod tabs;
 mod testing;
 
@@ -98,7 +99,13 @@ pub fn main() {
     let opts = Opts::from_args();
     println!("### Running sync integration tests ###");
     init_testing();
-    run_test_groups(&opts, vec![crate::logins::get_test_group()]);
-    run_test_groups(&opts, vec![crate::tabs::get_test_group()]);
+    // Commenting these out to make the test runs shorter and less
+    // noisy. It would be nice to add an option to `Opts` to only
+    // run certain groups.
+    //
+    // run_test_groups(&opts, vec![crate::logins::get_test_group()]);
+    // run_test_groups(&opts, vec![crate::tabs::get_test_group()]);
+
+    run_test_groups(&opts, vec![crate::sync15::get_test_group()]);
     println!("### Sync integration tests passed!");
 }
